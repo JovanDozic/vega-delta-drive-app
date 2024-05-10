bce5a250525dabd1b3e81f1c64d5e69c53eb465c
Jovan Dozic <jovandozic@gmail.com>
2024-05-10 15:55:27 +0200

    Added routing and default components.

--- DeltaDrive-Frontend/src/app/app-routing.module.ts
+++ DeltaDrive-Frontend/src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { RegistrationComponent } from './authentication/registration/registration.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // TODO: canActivate: [authenticationGuard]
 
   { path: 'home', component: HomeComponent },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistrationComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- DeltaDrive-Frontend/src/app/app.module.ts
+++ DeltaDrive-Frontend/src/app/app.module.ts
@@ -4,17 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { LoginComponent } from './authentication/login/login.component';
+import { RegistrationComponent } from './authentication/registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(withFetch()),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
